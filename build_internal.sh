b642d743f94290e9fcd692799347109021b6ba07
tto <torsten.thau@cape-it.de>
2021-03-05 12:53:11 +0100

    added inactive default configuration for KIXPro-repositories

---
+++ build_internal.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+cd docker
+
+#export BACKEND_BUILD_TAG=build-rel-18
+#export FRONTEND_BUILD_TAG=build-rel-18
+#export IMAGE_TAG=stable
+#./internal.sh
+#
+#export BACKEND_BUILD_TAG=build-rel-18_TEST
+#export FRONTEND_BUILD_TAG=build-rel-18_TEST
+#export IMAGE_TAG=latest
+#./internal.sh
+#
+#export BACKEND_BUILD_TAG=build-rel-18_FEATURE
+#export FRONTEND_BUILD_TAG=build-rel-18_FEATURE
+#export IMAGE_TAG=dev
+#./internal.sh
+
+export BACKEND_BUILD_TAG=build-kix17-migration
+export FRONTEND_BUILD_TAG=build-rel-18_FEATURE
+export IMAGE_TAG=exp
+./internal.sh
+
+
