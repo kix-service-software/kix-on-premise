2254135918591409ee5f1d42dc41a495bce79a2e
Tobias Czirfusz <tobias.czirfusz@cape-it.de>
2022-03-22 09:18:39 +0100

    update windows deployment scripts

--- deploy/windows/update.ps1
+++ deploy/windows/update.ps1
@@ -2,6 +2,14 @@
     Set-Item -path "env:$($_.split('=')[0])" -value $_.split('=')[1] -Force;
 }
 
+docker pull $env:REGISTRY/ssp:$env:IMAGE_TAG 2>&1>$null
+if($?) {
+    Set-Item -path "env:COMPOSE_FILE" -value "docker-compose.yml:docker-compose_ssp.yml" -Force;
+}
+else {
+  Set-Item -path "env:COMPOSE_FILE" -value "docker-compose.yml" -Force;
+}
+
 docker-compose -p $env:NAME pull
 docker-compose -p $env:NAME up -d
 docker-compose -p $env:NAME restart proxy
