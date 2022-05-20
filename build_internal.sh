8cfbb541a107929f6ca95c2fdad280504550b823
Torsten Thau <torsten.thau@cape-it.de>
2022-05-20 09:47:11 +0000

    Update README.md

---
+++ build_internal.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cd docker
+
+#export BACKEND_BUILD_TAG=build-rel-18
+#export FRONTEND_BUILD_TAG=build-rel-18
+#export IMAGE_TAG=stable
+#./internal.sh
+#
+#export BACKEND_BUILD_TAG=build-rel-18_TEST
+#export FRONTEND_BUILD_TAG=build-rel-18_TEST
+#export IMAGE_TAG=latest
+#./internal.sh
+#
+#export BACKEND_BUILD_TAG=build-rel-18_FEATURE
+#export FRONTEND_BUILD_TAG=build-rel-18_FEATURE
+#export IMAGE_TAG=dev
+#./internal.sh
+
+export BACKEND_BUILD_TAG=build-rel-18
+export FRONTEND_BUILD_TAG=build-rel-18
+export SSP_BUILD_TAG=build-rel-18
+export IMAGE_TAG=rc
+./internal.sh
+
+
