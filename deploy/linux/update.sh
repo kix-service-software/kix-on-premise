93b42868eba1642539d13efad300a6c6ec6023b6
Torsten Thau <torsten.thau@cape-it.de>
2022-09-29 12:55:47 +0000

    Docker Compose as stand alone required
    (cherry picked from commit d5bf0fc5fd52697cd4f3cac1e307bd6ca3315327)

---
+++ deploy/linux/update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# import environment for docker-compose
+source ./environment
+export $(cut -d= -f1 ./environment | egrep '^[A-Z]')
+
+# check if SSP is available
+RESULT=$(docker pull ${REGISTRY}/ssp:${IMAGE_TAG} 2>&1 > /dev/null)
+if [ $? == 0 ]; then
+   export COMPOSE_FILE=docker-compose.yml:docker-compose_ssp.yml
+fi
+
+docker-compose -p ${NAME} pull 
+docker-compose -p ${NAME} up -d $@
+docker-compose -p ${NAME} restart proxy
