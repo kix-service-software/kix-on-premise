5641ceabc6d8fbefe411078fcde9bf9e214820c0
Tobias Czirfusz <tobias.czirfusz@cape-it.de>
2023-04-18 10:20:27 +0200

    Update .gitlab-ci.yml file

---
+++ README.md
@@ -0,0 +1,3 @@
+# KIX on-premise
+
+See `README.md` in `deploy/windows` or `deploy/linux` directory to learn how to deploy KIX on-premise.
---
+++ deploy/windows/logs.ps1
@@ -0,0 +1,13 @@
+(Get-Content .\environment) -cmatch '^[A-Z]' | ForEach-Object {
+    Set-Item -path "env:$($_.split('=')[0])" -value $_.split('=')[1] -Force;
+}
+
+docker pull $env:REGISTRY/ssp:$env:IMAGE_TAG 2>&1>$null
+if($?) {
+    Set-Item -path "env:COMPOSE_FILE" -value "docker-compose.yml:docker-compose_ssp.yml" -Force;
+}
+else {
+  Set-Item -path "env:COMPOSE_FILE" -value "docker-compose.yml" -Force;
+}
+
+docker-compose -p $env:NAME logs --follow
