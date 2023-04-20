03675ac9b7ab3d0c40c01d276e17c3ee5bbea957
root <root@docker.vm.devel.cape-it.de>
2023-04-20 09:58:21 +0200

    some changes from others

--- build_internal.sh
+++ build_internal.sh
@@ -17,12 +17,10 @@ cd docker
 #export IMAGE_TAG=dev
 #./internal.sh
 
-export BACKEND_BUILD_TAG=build-KIX2018-6492
-export FRONTEND_BUILD_TAG=build-KIX2018-6492
-export SSP_BUILD_TAG=build-KIX2018-7557
-export FRONTEND_PLUGINS=KIXPro:build-rel-18_FEATURE
-export BACKEND_PLUGINS=KIXPro:build-KIX2018-6492
-export IMAGE_TAG=dev
+export BACKEND_BUILD_TAG=build-rel-18_RC
+export FRONTEND_BUILD_TAG=build-rel-18_RC
+export SSP_BUILD_TAG=build-rel-18_RC
+export IMAGE_TAG=rc
 ./internal.sh
 
 
